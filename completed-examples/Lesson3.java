074697f58f7072d6de28ec4b1d41ab5f89c112e3
Aqc1019 <aqc1019@gmail.com>
2025-11-16 21:46:06 -0500

    Update Lesson3.java

--- completed-examples/Lesson3.java
+++ completed-examples/Lesson3.java
@@ -1,3 +1,27 @@
-void main() {
-    
+public class Main
+{
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void showSum(int a, int b) {
+        System.out.println("The sum is: " + (a + b));
+    }
+
+    public static double calculateAverage(double x, double y, double z) {
+        return (x + y + z) / 3.0;
+    }
+
+    public static void main(String[] args) {
+        
+        int num1 = 5;
+        int num2 = 7;
+        int result = add(num1, num2);
+        System.out.println("Result of add: " + result);
+
+        showSum(10, 20);
+
+        double avg = calculateAverage(6.0, 8.0, 10.0);
+        System.out.println("Average: " + avg);
+    }
 }
